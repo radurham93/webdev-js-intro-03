b48dd25a79c0a78de25a256552e0b317fbaf25b0
Robert Durham <radurham93@gmail.com>
2024-12-23 10:47:00 -0500

    did assignment

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+console.log(1+12); //output: 2
+console.log(2 + 2 +"5"); // output: 45
 
+
+console.log(1==2); //output: false
+console.log(1=="1"); //output: true
+console.log(1 === "1"); //output: false
+console.log(1 === 1) //output: true
+
+console.log(1 || 3) //output: 1
+console.log(3 || 2 || 5); //output:3
+
+const a = 3;
+const b = 5;
+
+console.log(a > 0 && b > 0); //output: true
+*/
